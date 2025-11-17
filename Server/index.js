4fe2293ee5ea4dd2fa22aeffaf3f5495fbb21429
Muzaffar Ameen <muzaffarameen389@gmail.com>
2025-11-18 01:36:35 +0500

    Third Commit

--- Server/index.js
+++ Server/index.js
@@ -75,6 +75,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use(cors({
+    origin: ["http://localhost:3000","https://paymentformapp.netlify.app/"]
+}));
+
 // Connect to MongoDB Atlas
 mongoose
   .connect(process.env.MONGO_URI)
