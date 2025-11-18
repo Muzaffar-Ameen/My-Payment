3974ad7e1f5bc8a530723cf42a176151c62e106c
Muzaffar Ameen <muzaffarameen389@gmail.com>
2025-11-18 16:07:55 +0500

    7th Commit

--- Client/src/pages/Login.jsx
+++ Client/src/pages/Login.jsx
@@ -1,12 +1,10 @@
 import React, { useState } from "react"
-import { Link } from "react-router-dom"
 import axios from "axios"
 import { useNavigate } from "react-router-dom"
 
 
 
 const Login = () => {
-    const [name, setName] = useState()
     const [email, setEmail] = useState()
     const [password, setPassword] = useState()
     const navigate = useNavigate()
